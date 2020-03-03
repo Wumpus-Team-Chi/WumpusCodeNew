55579d56accc5dc0bfa3509fd8e5f06a4a63b699
s-syadav <61330167+s-syadav@users.noreply.github.com>
2020-03-03 11:16:01 -0800

    Update Cave.cs

--- HuntTheWumpus/HuntTheWumpus/Cave.cs
+++ HuntTheWumpus/HuntTheWumpus/Cave.cs
@@ -1,44 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace HuntTheWumpus
+
+namespace MyWumpusWork
 {
     class Cave
     {
         //instance variables
-        private Room[,] myCave = new Room[5, 6];
-
+        private Room[][] myCave = new Room[5][];
         //constructors
-        public Cave(String File)
+        public Cave(String fileName)
         {
-            for(int i = 0; i < 5; i++)
+            for (int i = 0; i < myCave.Length; i++)
             {
-                for(int j = 0; j < 6; j++)
-                {
-                    myCave[i, j] = new Room();
-                }
+                myCave[i] = new Room[6];
             }
-            //generate a cave from a file
-            //fill Cave
+            //read the file
+            //depending on the number in the array, put a room object in the array
+            //for the room object use the file to change the room identity. 
+            //pick 2 rooms at random to have bats
+            //pick 1 room at random to have a bottomless pit
+            //pick 1 room at random to have a wumpus
+            
+
         }
         //Probably gonna delete this constructor if I cant end up figuring it out. 
         public Cave()
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < myCave.Length; i++)
             {
-                for (int j = 0; j < 6; j++)
-                {
-                    myCave[i, j] = new Room();
-                }
+                myCave[i] = new Room[6];
             }
-            Console.WriteLine("asdhfffhasdfhasdfhasdhfasdhfasdhf");
-            //randomly generate a cave
+            //assign each element in the array some room object with some other number. Ensure each number is unique
+            //pick 2 rooms at random to have bats
+            //pick 1 room at random to have a bottomless pit
+            //pick 1 room at random to have a wumpus
+
+
         }
         //mutators
-            //cave will remain static throughout. 
+        //cave will remain static throughout. 
         //accesors
         public int[] adjacentRooms()
         {
@@ -59,6 +64,48 @@ namespace HuntTheWumpus
             // 0 will be closed room
             // other values will indicate what room openings are available 
             return accessibleRooms; 
+        }
+        public int[] getBatRooms()
+        {
+            int room1 = -1;
+            int room2 = -2;
+            int[] batRoomNumbers = new int[2];
+            for (int i = 0; i < myCave.Length; i++)
+            {
+                for (int j = 0; j < myCave[i].Length; j++)
+                {
+                    int r = myCave[i][j].getRoomNum();  
+                    if (myCave[i][j].isBatPresent(r))
+                    {
+                        if (room1 != room2 && room1 < 0)
+                        {
+                            room1 = myCave[i][j].getRoomNum();
+                        }else if(room2 < 0)
+                        {
+                            room2 = myCave[i][j].getRoomNum();
+                        }
+                    }
+                }
+            }
+            batRoomNumbers[0] = room1;
+            batRoomNumbers[1] = room2;
+            return batRoomNumbers; 
+        }
+        public int getWumpusRoom()
+        {
+        
+            for (int i = 0; i < myCave.Length; i++)
+            {
+                for (int j = 0; j < myCave[i].Length; j++)
+                {
+                    int r = myCave[i][j].getRoomNum();
+                    if (myCave[i][j].isWumpusPresent(r))
+                    {
+                        return r;
+                    }
+                }
+            }
+            return -1; // if it gets here something in cave generation or wumpus assignment went wrong. 
         }
     }
 }
